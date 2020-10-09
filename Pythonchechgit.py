bef9861bacd99131cdfdde3f087e6d1a3d326afd
vikasheie <vikash.v3@gmail.com>
2020-10-09 19:20:37 +0530

    Selenium check

--- Pythonchechgit.py
+++ Pythonchechgit.py
@@ -1,3 +1,17 @@
-print("Testing jenkins git")
-print("Added new line")
-print("new line")
+from Screenshot import Screenshot_Clipping
+from selenium import webdriver
+from time import sleep
+
+from selenium.webdriver import ActionChains
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+
+
+
+driver=webdriver.Chrome("E:\\chromedriver\\chromedriver.exe")
+driver.maximize_window()
+driver.implicitly_wait(10)
+url = "https://www.google.com"
+driver.get(url)
+sleep(10)
+driver.quit()
